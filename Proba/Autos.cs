8d46fd838c32660bdab1b0005a63fda5c4d120a9
Alinli17 <63418576+Alinli17@users.noreply.github.com>
2021-04-18 13:31:52 +0300

    Отредактировала класс Autos

--- Proba/Autos.cs
+++ Proba/Autos.cs
@@ -8,5 +8,24 @@ namespace Proba
 {
     class Autos
     {
+        string name = ""; // создаются поля 
+        string marka = "";
+        int price = 0;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public string Marka
+        {
+            get { return this.marka; }
+            set { this.marka = value; }
+        }
+        public int Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
     }
 }
