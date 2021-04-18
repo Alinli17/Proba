688ca01472cf7e44aa5f7a7785d214368abb2cfe
Alinli17 <63418576+Alinli17@users.noreply.github.com>
2021-04-18 14:48:21 +0300

    Создан метод для записи автомобилей

--- Proba/Autos.cs
+++ Proba/Autos.cs
@@ -9,20 +9,20 @@ namespace Proba
     class Autos
     {
         string name = ""; // создаются поля 
-        string marka = "";
+        string model = "";
         int price = 0;
 
-        public string Name
+        public string Name // метод для поля name, чтобы получать и записывать(устанавливать) значения
         {
             get { return this.name; }
             set { this.name = value; }
         }
-        public string Marka
+        public string Model // метод для поля marka, чтобы получать и записывать(устанавливать) значения
         {
-            get { return this.marka; }
-            set { this.marka = value; }
+            get { return this.model; }
+            set { this.model = value; }
         }
-        public int Price
+        public int Price // метод для поля price, чтобы получать и записывать(устанавливать) значения
         {
             get { return this.price; }
             set { this.price = value; }
--- Proba/Program.cs
+++ Proba/Program.cs
@@ -8,9 +8,84 @@ namespace Proba
 {
     class Program
     {
+        private static int n; // размер массива
+        private static Autos[] Autos; // создали мвссив Autos (пустой)
+
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("Введите размер массива: ");
+                string n1 = Console.ReadLine();
 
+                while(!Int32.TryParse(n1, out n)) // проверка на введенное целое число
+                {
+                    Console.WriteLine("Введите целое число!");
+                    Console.Write("Введите размер массива: ");
+                    n1 = Console.ReadLine();
+                }
+
+                Autos = new Autos[n]; // создали массив с n элементами
+                ReadAutos();
+
+
+
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static private void ReadAutos()
+        {
+            for (int i = 0;  i < n; i++)
+            {
+                Autos[i] = new Autos();
+
+                Console.Write("Введите название машины {0}: ", i + 1);
+                string name = Console.ReadLine();
+
+                while (string.IsNullOrEmpty(name)) // проверка на то, что что-то ввели (не пусто)
+                {
+                    Console.Write("Введите название машины {0}: ", i + 1);
+                    name = Console.ReadLine();
+                }
+
+                Autos[i].Name = name;
+
+                Console.Write("Введите марку машины {0}: ", i + 1);
+                string model = Console.ReadLine();
+
+                while (string.IsNullOrEmpty(model)) // проверка на то, что что-то ввели (не пусто)
+                {
+                    Console.Write("Введите марку машины {0}: ", i + 1);
+                    model = Console.ReadLine();
+                }
+
+                Autos[i].Model = model;
+
+                Console.Write("Введите цену машины {0}: ", i + 1);
+                string price1 = Console.ReadLine();
+                int price;
+
+                while (string.IsNullOrEmpty(price1)) // проверка на то, что что-то ввели (не пусто)
+                {
+                    Console.Write("Введите цену машины {0}: ", i + 1);
+                    price1 = Console.ReadLine();
+                }
+
+                while (!Int32.TryParse(price1, out price)) // проверка, если ввели букву, а не цифру
+                {
+                    Console.WriteLine("Введите целое число!");
+                    Console.Write("Введите размер массива: ");
+                    price1 = Console.ReadLine();
+                }
+
+                Autos[i].Price = price;
+            }
         }
     }
 }
